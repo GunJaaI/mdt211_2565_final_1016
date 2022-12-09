a8e6c4deaa3001b72f29c7cfc8f7f2dc3116fc81
GunJaaI <110761756+GunJaaI@users.noreply.github.com>
2022-12-09 15:06:44 +0700

    09/12/65

---
+++ 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<char> calculate = new Queue<char>();
+        int x = 0;
+        int y = 0;
+        while(true)
+        {
+            Console.Write("-> ");
+            char queue = char.Parse(Console.ReadLine());
+            if (queue == '(') {
+                calculate.Push(queue);
+                x++;
+            } else if (queue == ')') {
+                calculate.Push(queue);
+                y++;
+            } else { break; }
+        }
+        if (x == y) {
+            Console.WriteLine("Valid");
+                
+        } else {
+            Console.WriteLine("Invalid");
+        }
+
+    }
+}
