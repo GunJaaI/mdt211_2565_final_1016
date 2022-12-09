e42f73a37e7235a9430979d198b5f85652783f5c
GunJaaI <110761756+GunJaaI@users.noreply.github.com>
2022-12-09 15:29:14 +0700

    09/12/65

---
+++ 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<string> calculate = new Queue<string>();
+        while (true) {
+            Console.Write("-> ");
+            string queue = Console.ReadLine();
+            if (queue == "Exit") {
+                break;
+            } else {
+                calculate.Push(queue);
+            }
+        }
+        Console.WriteLine(calculate.Get(calculate.GetLength() - 3));
+        Console.WriteLine(calculate.Get(calculate.GetLength() - 2));
+        Console.WriteLine(calculate.Get(calculate.GetLength() - 1));
+
+    }
+}
